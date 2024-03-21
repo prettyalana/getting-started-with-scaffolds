c1eb0c55ee28c3839082718d6033f8494159a476
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-03-21 20:24:10 +0000

    Updates

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,67 @@
+class MoviesController < ApplicationController
+  def new
+    @the_movie = Movie.new
+    render template: "movies/new"
+  end
+  def edit
+    @the_movie = Movie.where(id: params.fetch(:id))[0]
+    render template: "movies/edit"
+  end
+  def index
+    matching_movies = Movie.all
+
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
+    render({ :template => "movies/index" })
+  end
+
+  def show
+    the_id = params.fetch("id")
+
+    matching_movies = Movie.where({ :id => the_id })
+
+    @the_movie = matching_movies[0]
+    # @the_movie = matching_movies.first is the same thing
+
+    render({ :template => "movies/show" })
+  end
+
+  def create
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
+
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies", { :notice => "Movie created successfully." })
+    else
+      render template: "movies/new"
+    end
+  end
+
+  def update
+    the_id = params.fetch("id")
+    @the_movie = Movie.where({ :id => the_id })[0]
+
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
+
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies/#{@the_movie.id}", { :notice => "Movie updated successfully."} )
+    else
+      render template: "movies/new"
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
+
+    the_movie.destroy
+
+    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+  end
+end
